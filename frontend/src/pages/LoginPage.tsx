ca58178e59e2e280a15cd4ecde73204ec0f8b013
Derek Finnell <121850273+DAFinnell@users.noreply.github.com>
2026-06-03 21:16:11 -0400

    Create frontend layout (#18)
    * remove unused App.css file
    
    * add react-router-dom and create folder structure/initial pages
    
    * add react-router-dom and create folder structure/initial pages pt2
    
    * add frontend skeleton
    
    * made simple shared app layout with basic nav bar and basic tailwind classes for frontend pages

---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,10 @@
+export default function LoginPage() {
+    return (
+        <section className="space-y-4">
+            <h1 className="text-3xl font-semibold tracking-tight">Login</h1>
+            <p className="text-slate-600">
+                Temporary frontend page for login. This will be replaced with the actual login page content in the future.
+            </p>
+        </section>
+    )
+}
